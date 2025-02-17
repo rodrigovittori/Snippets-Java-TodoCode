07cf3b22643f6be773e6e1b311f88852e96dde54
Rodrigo Vittori <119360928+rodrigovittori@users.noreply.github.com>
2025-02-17 01:23:42 -0300

    1-1 "Hola, Mundo!"
    Dicen que el primer paso es siempre el más difícil... esta vez no fue así, ¡pero que no se pierda el saludo che!

---
+++ JavaParaPrincipiantes/1-HolaMundo/src/main/java/educational/holamundo/App.java
@@ -0,0 +1,20 @@
+package educational.holamundo;
+
+/** Actividad práctica del curso "Java para Principiantes" de TODO CODE ACADEMY
+ * 
+ * Clase Nº 1 - "¿Qué es Programar?"
+ * 
+ * Link video: https://www.youtube.com/watch?v=6q7wHs6cH6Y
+ * 
+ * ----------------------------------------------------------------------------
+ * 
+ * @author rodrigovittori // github.com/rodrigovittori
+ */
+
+public class App {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hola, Mundo!");
+    }
+}
